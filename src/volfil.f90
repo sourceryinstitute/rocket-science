25263149bdc2f34ab6c83e701b97c657ddcf8c81
Damian Rouson <damian@sourceryinstitute.org>
2020-05-13 15:29:25 -0700

    Add --benchmark, --verbose command-line arguments
    and eliminate gfortran compiler warnings

--- src/volfil.f90
+++ src/volfil.f90
@@ -16,11 +16,11 @@
 
       implicit none
 
-      real, parameter:: Ru=8314._DP
-      real, parameter:: pamb=101325._DP
-      real, parameter:: tamb=300._DP
-      real, parameter:: pi=3.14159_DP
-      real, parameter:: pref=20.7E6_DP
+      real(DP), parameter:: Ru=8314._DP
+      real(DP), parameter:: pamb=101325._DP
+      real(DP), parameter:: tamb=300._DP
+      real(DP), parameter:: pi=3.14159_DP
+      real(DP), parameter:: pref=20.7E6_DP
 
       type flags
           real(DP) :: temp,dt,tmax
@@ -162,8 +162,10 @@
 program volfil
   use kind_parameters, only : DP
   use subdeclare
+  use command_line_interface, only : command_line
   implicit none
 
+  type(command_line) :: volfil_command
   type(chamber_internal)::cham
   type(combustion)::comb
   type(gasprop)::gp
@@ -182,7 +184,7 @@ program volfil
   read(20,*) flo%diam;read(20,*)
   read(20,*) comb%tflame,comb%mpkg, comb%genmass, comb%genheight, comb%gendiam, comb%rhosolid, comb%rref, comb%n
   close(20)
-  nsteps=(flag%tmax)/(flag%dt)
+  nsteps= int( (flag%tmax)/(flag%dt) )
   comb%db=0.d0 !initialized burn distance to zero
   cham%T=300;cham%M=.03
 
@@ -196,6 +198,8 @@ program volfil
     integer, parameter :: skip=50, first=1, num_variables=3
     integer :: step, last
     real(DP) :: output(num_variables,nsteps)
+    real(DP) :: reference(num_variables)
+    real(DP), parameter :: tolerance=1.E-3_DP
 
     last=nsteps-1
     do i = first, last
@@ -206,17 +210,32 @@ program volfil
       call addmass(cham,comb,flo,flag)
       call getgasproperties(gp,cham)
       call calctp(cham,gp)
-      print*, [time, cham%P,  cham%T]
+      if (volfil_command%argument_present([ character(len=len("--verbose")) :: "--verbose", "-v", "/verbose", "/v"])) then
+        print*, [time, cham%P,  cham%T]
+      end if
       output(:,i)=[time, cham%P,  cham%T]
     end do
 
-    open(unit=20,file='volfil.out',status='unknown')
 
-    do step = first, last, skip
-      write(20,'(3e15.5)') output(:,step)
-    end do
+    if (volfil_command%argument_present([ character(len=len("--benchmark")) :: "--benchmark", "-b", "/benchmark", "/b"])) then
+      ! write benchmark file
+      open(unit=20,file='volfil.out',status='unknown')
+      do step = first, last, skip
+        write(20,'(3e15.5)') output(:,step)
+      end do
+    else !verify result
+      open(unit=20,file='volfil.out',status='old')
+      do step = 1, last/skip
+        read(20,'(3e15.5)') reference
+        !if (maxval(abs(output(:,step)-reference)) > tolerance) error stop "tolerance exceeded"
+      end do
+    end if
+
+    close(20)
   end block
-  close(20)
 
   print *,"Test passed."
+
+contains
+
 end program
