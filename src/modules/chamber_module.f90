860ae0606277119c5a0c5f0d20468b2045357d25
Damian Rouson <damian@sourceryinstitute.org>
2020-06-19 18:43:53 -0700

    make test-chamber.f90 pass

--- src/modules/chamber_module.f90
+++ src/modules/chamber_module.f90
@@ -1,9 +1,42 @@
 module chamber_module
+  use kind_parameters, only : DP
   implicit none
-  private
 
+  private
   public :: chamber_t
+  public :: define
+  public :: get_volume
 
   type chamber_t
+    private
+    real(DP) :: volume
   end type
+
+contains
+
+  subroutine define(this, file_name)
+    use assertions_interface, only : max_errmsg_len
+    type(chamber_t), intent(out) :: this
+    character(len=*), intent(in) :: file_name
+    character(len=max_errmsg_len) error_message
+    real(DP) :: volume
+    integer :: io_status, file_unit
+    integer, parameter :: success = 0
+    namelist/chamber/ volume
+
+    open(newunit=file_unit, file=file_name, status="old", iostat=io_status, iomsg=error_message)
+    if (io_status /= success) error stop "chamber%define:  "
+    read(file_unit, nml=chamber)
+    this%volume = volume
+
+    close(file_unit)
+
+  end subroutine
+
+  function get_volume(this) result(this_volume)
+    type(chamber_t), intent(in) :: this
+    real(DP) :: this_volume
+    this_volume = this%volume
+  end function
+
 end module chamber_module
--- tests/unit/chamber/test-chamber.f90
+++ tests/unit/chamber/test-chamber.f90
@@ -1,1 +1,13 @@
-end
+program main
+  use chamber_module, only : chamber_t, define, get_volume
+  use kind_parameters, only : DP
+  implicit none
+  type(chamber_t) chamber
+  real, parameter :: expected_volume=10.E-04_DP, tolerance=1.E-06
+
+  call define(chamber, "volfil.inp")
+
+  if ( abs((get_volume(chamber) - expected_volume)/expected_volume) > tolerance)  error stop "Test failed."
+
+  print *,"Test passed."
+end program
