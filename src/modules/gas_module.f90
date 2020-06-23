0f6ec9296f1d8dfc0588f559728f22c8d3f37734
Damian Rouson <damian@sourceryinstitute.org>
2020-06-22 20:42:49 -0700

    white space edit: add space after comma separators

--- src/modules/gas_module.f90
+++ src/modules/gas_module.f90
@@ -1,6 +1,6 @@
 module gas_module
   use kind_parameters, only : DP
-  
+
   implicit none
   private
 
@@ -10,26 +10,26 @@ module gas_module
 
   type gas_t
     private
-    real(DP) :: c_p,T,MW,m 
+    real(DP) :: c_p, T, MW, m
   end type
 
 contains
-        
+
   subroutine defineg(this, file_name)
     use assertions_interface, only : max_errmsg_len
     type(gas_t), intent(out) :: this
     character(len=*), intent(in) :: file_name
     character(len=max_errmsg_len) error_message
-    real(DP) :: c_p,T,MW,m 
+    real(DP) :: c_p, T, MW, m
     integer :: io_status, file_unit
     integer, parameter :: success = 0
-    namelist/gas/ c_p,MW,T,m 
+    namelist/gas/ c_p, MW, T, m
 
     open(newunit=file_unit, file=file_name, status="old", iostat=io_status, iomsg=error_message)
     if (io_status /= success) error stop "gas%define:  "
     read(file_unit, nml=gas)
     this%c_p = c_p
- !   this%MW = MW 
+ !   this%MW = MW
  !   this%T = T
  !   this%m = m
 
