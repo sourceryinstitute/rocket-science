373fe669842cb2d24c5ecc9d613edaaab4168d3a
Damian Rouson <damian@sourceryinstitute.org>
2020-06-27 22:04:45 -0700

    unit tests: switch if/then to "call assert(...)"

--- tests/unit/hole/test-hole.f90
+++ tests/unit/hole/test-hole.f90
@@ -1,15 +1,16 @@
 program main
+  use assertions_interface, only : assert
   use hole_module, only :  hole_t, define, get_diameter
   use kind_parameters, only : DP
   implicit none
   type (hole_t) hole
-  real, parameter :: expected_diameter=9.E-3
-  real, parameter :: tolerance=1.E-6
+  real(DP), parameter :: expected_diameter=9.E-3_DP
+  real(DP), parameter :: tolerance=1.E-6_DP
 
   call define(hole, "volfil.inp")
 
-  if(abs((get_diameter(hole) - expected_diameter)/expected_diameter) > tolerance) error stop "Test Failed (hole)"
-
+  call assert(abs((get_diameter(hole) - expected_diameter)/expected_diameter) <= tolerance, &
+             "abs((get_diameter(hole) - expected_diameter)/expected_diameter) <= tolerance")
 
   print *, "Test passed"
 end program
