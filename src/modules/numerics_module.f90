401f4bbf605beef0efd34fa0b461523cf4d99d53
Damian Rouson <damian@sourceryinstitute.org>
2020-07-10 13:54:03 -0700

    rebase against master

--- src/modules/numerics_module.f90
+++ src/modules/numerics_module.f90
@@ -1,4 +1,5 @@
 module numerics_module
+  use assertions_interface, only : assert
   use kind_parameters, only : DP
   implicit none
   private
@@ -7,39 +8,46 @@ module numerics_module
   public :: define
   public :: get_dt
   public :: get_tmax
+  public :: get_time
+  public :: set_time
+  public :: operator(+)
+  public :: operator(*)
+  public :: d_dt
 
   type numerics_t
-    private
-    real(DP) dt, tmax, time
+  private
+  real(DP) dt, tmax, time
   end type
+
+  interface operator(+)
+    module procedure add
+  end interface
+
+  interface operator(*)
+    module procedure multiply
+  end interface
 
 contains
 
   subroutine define(this, file_name)
-    use assertions_interface, only : max_errmsg_len
-    type(numerics_t), intent(out) :: this
-    character(len=*), intent(in) :: file_name
-    character(len=max_errmsg_len) error_message
-    real(DP) :: dt, tmax, time
-    integer :: io_status, file_unit
-    integer, parameter :: success =0
-    namelist/numerics/ dt, tmax
+   use assertions_interface, only : max_errmsg_len
+   type(numerics_t), intent(out) :: this
+   character(len=*), intent(in) :: file_name
+   character(len=max_errmsg_len) error_message
+   real(DP) :: dt, tmax, time
+   integer :: io_status, file_unit
+   integer, parameter :: success =0
+   namelist/numerics_list/ dt, tmax
 
-    open(newunit=file_unit, file=file_name, status="old", iostat=io_status, iomsg=error_message)
-    if (io_status /= success) error stop "chamber%define:  "
-    read(file_unit, nml=numerics)
-    this%dt=dt
-    this%tmax=tmax
+   open(newunit=file_unit, file=file_name, status="old", iostat=io_status, iomsg=error_message)
+   call assert(io_status == success, "define(numerics): io_status == success", error_message)
+   read(file_unit, nml=numerics_list)
+   this%dt=dt
+   this%tmax=tmax
 
-    close(file_unit)
+   close(file_unit)
 
-   end subroutine
-
-   function get_tmax(this) result(this_tmax)
-     type(numerics_t), intent(in) :: this
-     real(DP) :: this_tmax
-     this_tmax=this%tmax
-   end function
+  end subroutine
 
    function get_dt(this) result(this_dt)
      type(numerics_t), intent(in) :: this
@@ -47,5 +55,47 @@ contains
      this_dt=this%dt
    end function
 
+   function get_tmax(this) result(this_tmax)
+     type(numerics_t), intent(in) :: this
+     real(DP) :: this_tmax
+     this_tmax=this%tmax
+   end function
+
+   function get_time(this) result(this_time)
+     type(numerics_t), intent(in) :: this
+     real(DP) this_time
+     this_time = this%time
+   end function
+
+   subroutine set_time(this, time)
+     type(numerics_t), intent(inout) :: this
+     real(DP), intent(in) :: time
+     this%time = time
+   end subroutine
+
+   function d_dt(this) result(dthis_dt)
+     type(numerics_t), intent(in) :: this
+     type(numerics_t) dthis_dt
+     dthis_dt%dt = 0._DP
+     dthis_dt%tmax = 0._DP
+     dthis_dt%time = 1._DP
+   end function
+
+   function add(lhs, rhs) result(total)
+     type(numerics_t), intent(in) :: lhs, rhs
+     type(numerics_t) total
+     total%dt = lhs%dt + rhs%dt
+     total%tmax = lhs%tmax + rhs%tmax
+     total%time = lhs%time + rhs%time
+   end function
+
+   function multiply(lhs, rhs) result(product)
+     type(numerics_t), intent(in) :: lhs
+     real(DP), intent(in) :: rhs
+     type(numerics_t) product
+     product%dt = lhs%dt * rhs
+     product%tmax = lhs%tmax * rhs
+     product%time = lhs%time * rhs
+   end function
 
 end module numerics_module
--- tests/unit/numerics/test-numerics.f90
+++ tests/unit/numerics/test-numerics.f90
@@ -1,17 +1,46 @@
 program main
-  use assertions_interface, only : assert
-  use numerics_module, only : numerics_t, define, get_dt, get_tmax
+  !! Test the numerics module's type, procedures, and operators
+  use assertions_interface, only : assert, max_errmsg_len
+  use numerics_module, only : numerics_t, define, get_dt, get_tmax, get_time, set_time, operator(+), operator(*), d_dt
   use kind_parameters, only : DP
   implicit none
   type(numerics_t) numerics
-  real(DP), parameter :: expected_dt=1e-6_DP, expected_tmax=0.1e0_DP, tol=1.e-6_DP
+  real(DP), parameter :: tol=1.e-6_DP
+  character(len=max_errmsg_len) error_message
+  character(len=*), parameter :: file_name="volfil.inp"
+  real(DP) :: dt, tmax
+  integer :: io_status, file_unit
+  integer, parameter :: success =0
+  namelist/numerics_list/ dt, tmax
 
-  call define(numerics, "volfil.inp")
+  ! Read dt and tmax from file
+  open(newunit=file_unit, file=file_name, status="old", iostat=io_status, iomsg=error_message)
+  call assert(io_status == success, "io_status == success", error_message)
+  read(file_unit, nml=numerics_list)
+  close(file_unit)
 
-  call assert(abs((get_dt(numerics)-expected_dt)/expected_dt) <= tol, &
-             "abs((get_dt(numerics)-expected_dt)/expected_dt) <= tol,")
-  call assert(abs((get_tmax(numerics)-expected_tmax)/expected_tmax) <= tol, &
-             "abs((get_tmax(numerics)-expected_tmax)/expected_tmax) <= tol")
+  ! Test define subroutine: check that it sets and stores the dt & tmax values defined in the input file
+  call define(numerics, file_name) !! initialize numerics object with data read from file
+  call assert(abs((get_dt(numerics)-dt)/dt) <= tol, &
+             "abs((get_dt(numerics)-dt)/dt) <= tol")
+  call assert(abs((get_tmax(numerics)-tmax)/tmax) <= tol, &
+             "abs((get_tmax(numerics)-tmax)/tmax) <= tol")
+
+  associate(t => get_time(numerics), dt => get_dt(numerics)) ! Fortran 2003
+    !! associate names with numerics function results to
+    !! 1. Eliminate the need for a declaration (making code more compact).
+    !! 2. Eliminate one class of mistakes (using the variable in a way that is problematic given its declared type).
+    !! 3. Enable compiler optimizations (because t & dt are immutable, expressions like t+dt need be evaluated only once).
+
+    call set_time(numerics, t + dt) !! increment time by dt using setter
+    call assert(abs((get_time(numerics)-(t+dt)))/(t+dt) <= tol, &
+               "abs((get_time(numerics)-(t+dt)))/(t+dt) <= tol")
+
+    numerics = numerics + d_dt(numerics)*dt !! increment time by dt using user-defined operators
+    call assert(abs((get_time(numerics)-(t+2*dt)))/(t+2*dt) <= tol, &
+               "abs((get_time(numerics)-(t+2*dt)))/(t+2*dt) <= tol")
+
+  end associate
 
   print *, "Test passed."
 end program main
