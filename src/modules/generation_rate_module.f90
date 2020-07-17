f84d7ef351c0c043ad2c0a316f0f12afb2198906
Damian Rouson <damian@sourceryinstitute.org>
2020-07-16 21:59:51 -0700

    encapsulate mass/energy generation rate

---
+++ src/modules/generation_rate_module.f90
@@ -0,0 +1,42 @@
+module generation_rate_module
+  use kind_parameters, only : DP
+  implicit none
+
+  private
+  public :: generation_rate_t
+  public :: define
+  public :: e_dot_gen
+  public :: m_dot_gen
+
+  type generation_rate_t
+    private
+    real(DP) e_dot_g
+    real(DP) m_dot_g
+  end type
+
+  interface define
+    module procedure define_generation_rate
+  end interface
+
+contains
+
+  subroutine define_generation_rate(this, mass_generation_rate, energy_generation_rate)
+    type(generation_rate_t), intent(out) :: this
+    real(DP), intent(in) :: mass_generation_rate, energy_generation_rate
+    this%m_dot_g = mass_generation_rate
+    this%e_dot_g = energy_generation_rate
+  end subroutine
+
+  function m_dot_gen(this) result(m_dot)
+    type(generation_rate_t), intent(in) :: this
+    real(DP) m_dot
+    m_dot = this%m_dot_g
+  end function
+
+  function e_dot_gen(this) result(e_dot)
+    type(generation_rate_t), intent(in) :: this
+    real(DP) e_dot
+    e_dot = this%e_dot_g
+  end function
+
+end module generation_rate_module
