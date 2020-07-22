21d647c57c608baab185284abd56067fceb9b90c
Damian Rouson <damian@sourceryinstitute.org>
2020-07-22 08:30:26 -0700

    add comment describing generic interface
    see generation_rate_module.f90

--- src/modules/generation_rate_module.f90
+++ src/modules/generation_rate_module.f90
@@ -20,7 +20,61 @@ module generation_rate_module
   end type
 
   interface generation_rate_t
+    !! This is a generic interface.  Generic interfaces provide an alias ("generation_rate_t")
+    !! to refer to one or more procedures.  Fortran 2003 allows generic interface names to have
+    !! the same name as the type.  A common pattern across object-oriented languages is to have
+    !! a collection of procedures that can be invoked using the name of the type of object that
+    !! they construct.  Such procedures are referred to as constructors. This is analogous to
+    !! the generic interface "define" used elsewhere in volfil.  The "define" generic interfaces
+    !! all refer to subroutines. In the case here, define_generation_rate is a function, which
+    !! is the more common choice for constructors in Fortran.
+    !!
+    !! It might be useful to know that Fortran 90 also provided default "structure constructors"
+    !! that the user didn't need to write because the language provided them.  The following code
+    !! is standard-conforming Fortran 90:
+    !!
+    !! module foo_module
+    !!   implicit none
+    !!
+    !!   type foo
+    !!     integer i
+    !!   end type
+    !!
+    !! end module
+    !!
+    !! program main
+    !!   use foo_module, only : foo
+    !!   implicit none
+    !!
+    !!   type(foo) bar
+    !!
+    !!   bar = foo(i=1)
+    !!   print *,bar%i
+    !! end program
+    !!
+    !! where the function foo() is provided by the language itself and gives a function result that
+    !! is a foo object with its i component set to 1.  The above program should print "1".
+    !! The language-provided structure constructor (the function "foo" above), however, proves
+    !! unuseful in object-oriented programming (OOP) because it only works if the
+    !! components are public, which violates the information-hiding principle of OOP. A solution
+    !! to this dilemma is to let users write their own structure constructors.  Fortran 2003
+    !! that by allowing generic interfaces to have a name matching the type name.
+    !!
+    !! Try saving the above program as main.f90 and compiling with
+    !!
+    !!   gfortran -std=f95 main.f90
+    !!   ./a.out
+    !!
+    !! Because Akin's book uses Fortran 90/95, he didn't have the ability to define generic interfaces
+    !! with names matching the corresponding type names so he added an underscore to the name of his
+    !! constructors.  So the first executable line in the above program would instead be
+    !!
+    !! bar = foo_(i=1)
+    !!
+    !! in the style of Akins.
     module procedure define_generation_rate
+      ! This can be a comma-separated list of procedures as long as each has
+      ! different argument lists.
   end interface
 
 contains
