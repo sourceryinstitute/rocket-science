80e4795e0bc1e90fab631f67df0c89dde237f825
Damian Rouson <damian@sourceryinstitute.org>
2020-07-30 22:05:18 -0700

    produce procedural output as function result

--- src/rocket/main.f90
+++ src/rocket/main.f90
@@ -6,13 +6,13 @@ program main
 
   interface
 
-    function rocket() result(output)
+    function reference_rocket() result(output)
       real(8), allocatable :: output(:,:)
     end function
 
   end interface
 
-  associate(reference_data => rocket())
+  associate(reference_data => reference_rocket())
     do i=1,size(reference_data,1)
       print *,reference_data(i,1), reference_data(i,2)
     end do
