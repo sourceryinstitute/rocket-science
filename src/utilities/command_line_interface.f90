cc556844ff334853b386cf97e20d129961e6217f
Damian Rouson <damian@sourceryinstitute.org>
2020-08-14 13:43:12 -0700

    Clarify command_line_interface comment

--- src/utilities/command_line_interface.f90
+++ src/utilities/command_line_interface.f90
@@ -12,7 +12,7 @@ module command_line_interface
 
   interface
     module function argument_present(acceptable_argument) result(found)
-      !! result is .true. iff a command-line argument matches an element of this function's argument
+      !! result is .true. only if a command-line argument matches an element of this function's argument
       character(len=*), intent(in) :: acceptable_argument(:)
         !! sample list: [character(len=len(<longest_argument>)):: "--benchmark", "-b", "/benchmark", "/b"]
         !! where dashes support Linux/macOS, slashes support Windows, and <longest_argument> must be replaced
