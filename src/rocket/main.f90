6427cbb6958f6a109e520fbc70068a303620297b
Damian Rouson <damian@sourceryinstitute.org>
2020-08-25 22:27:41 -0700

    modularize main program: convert blocks to interal subroutines

--- src/rocket/main.f90
+++ src/rocket/main.f90
@@ -1,23 +1,9 @@
 program main
   !! Test the new rocket motor simulator against the legacy simulator
-  use assertions_interface, only : assert, max_errmsg_len
   use motor_module, only : motor_t
   use state_module, only : state_t
   use kind_parameters, only : rkind
   implicit none
-
-  real, parameter :: tolerance=1.E-6
-
-  interface
-
-    function legacy_rocket(input_file)
-      use results_interface, only : results_t
-      implicit none
-      character(len=*), intent(in) :: input_file
-      type(results_t) legacy_rocket
-    end function
-
-  end interface
 
   real(rkind), parameter :: zero=0._rkind
   character(len=*), parameter :: input_file="rocket.inp"
@@ -25,7 +11,6 @@ program main
   type(motor_t) motor
   type(state_t) state !! state variables updated at each time step: mass, energy, time, and burn depth
   type(state_t), allocatable :: history(:)
-  character(len=*), parameter :: header(*) = [ character(len=len("burn_depth")) :: "time", "mass", "energy", "burn_depth"]
 
   call motor%define(input_file)
 
@@ -34,40 +19,54 @@ program main
   end associate
 
   associate(dt => motor%dt() )
-
     history = [state]
-
     do while(state%time() < motor%t_max())
       state = state + motor%d_dt(state)*dt
       history = [history, state]
     end do
-
-    !associate(reference_results => legacy_rocket(input_file))
-      block
-        use command_line_interface, only : command_line_t
-        use results_interface, only : results_t
-        type(command_line_t) command
-        character(len=max_errmsg_len) error_message
-        integer io_status, file_unit
-        integer, parameter :: success = 0
-
-        if (command%argument_present([character(len=len("--graph")):: "--graph", "-g", "/graph", "/g"])) then
-
-          open(newunit=file_unit, file="rocket.out", status="unknown", iostat=io_status, iomsg=error_message)
-          call assert(io_status == success, "main (opening rocket.out): io_status == success", diagnostic_data=error_message)
-          write(unit=file_unit, fmt=*) results_t(header, history)
-          close(file_unit)
-
-          open(newunit=file_unit, file="legacy_rocket.out", status="unknown", iostat=io_status, iomsg=error_message)
-          call assert(io_status == success, "main (opening legacy_rocket.out): io_status == success", diagnostic_data=error_message)
-          write(unit=file_unit, fmt=*) legacy_rocket(input_file)
-          close(file_unit)
-
-          call execute_command_line('gnuplot gnuplot.inp')
-        end if
-      end block
-    !end associate
   end associate
 
+  call write_histories
+  call graph_results_if_requested
+
   print *,"Test passed."
+
+contains
+
+  subroutine graph_results_if_requested
+    use command_line_interface, only : command_line_t
+    type(command_line_t) command
+    character(len=*), parameter :: graph(*) = [character(len=len("--graph")):: "--graph", "-g", "/graph", "/g"]
+
+    if (command%argument_present(graph)) call execute_command_line('gnuplot gnuplot.inp')
+  end subroutine
+
+  subroutine write_histories
+    use assertions_interface, only : assert, max_errmsg_len
+    use results_interface, only : results_t
+    character(len=*), parameter :: header(*) = [ character(len=len("burn_depth")) :: "time", "mass", "energy", "burn_depth"]
+    character(len=max_errmsg_len) error_message
+    integer io_status, file_unit
+    integer, parameter :: success = 0
+
+    interface
+      function legacy_rocket(input_file)
+        use results_interface, only : results_t
+        implicit none
+        character(len=*), intent(in) :: input_file
+        type(results_t) legacy_rocket
+      end function
+    end interface
+
+    open(newunit=file_unit, file="rocket.out", status="unknown", iostat=io_status, iomsg=error_message)
+    call assert(io_status == success, "main (opening rocket.out): io_status == success", diagnostic_data=error_message)
+    write(unit=file_unit, fmt=*) results_t(header, history)
+    close(file_unit)
+
+    open(newunit=file_unit, file="legacy_rocket.out", status="unknown", iostat=io_status, iomsg=error_message)
+    call assert(io_status == success, "main (opening legacy_rocket.out): io_status == success", diagnostic_data=error_message)
+    write(unit=file_unit, fmt=*) legacy_rocket(input_file)
+    close(file_unit)
+  end subroutine
+
 end program
